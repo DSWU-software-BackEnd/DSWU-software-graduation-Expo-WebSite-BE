2d3ed8e151ff764a159be22dadf06eded1e18740
seoheeJeong <917sunny917@gmail.com>
2024-05-20 11:41:16 +0900

    crud

--- blog/Blog/Blog/urls.py
+++ blog/Blog/Blog/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from BlogApp import views #BlogApp 앱의 view import
+from django.urls import path, include
+#from BlogApp import views #BlogApp 앱의 view import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.list, name='list') #루트 경로
+    path('', include('BlogApp.urls')) #루트 경로로 요청이 왔을 때 list함수 요청하겠다
     ]
--- blog/Blog/BlogApp/views.py
+++ blog/Blog/BlogApp/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
 def list(request):
     posts = Post.objects.all().order_by('-id') # Post 객체를 내림차순으로 모두 불러온 후에 posts 변수에 담음
     return render(request, 'list.html', {'posts' : posts}) # blog/index.html에서 posts 변수명으로 사용할 수 있도록!
 
+# CRUD
+def create(request):
+    if request.method == 'POST':   # 사용자의 요청이 POST라면
+        title = request.POST.get('title') # 사용자가 입력한 title, content 데이터에서 'title', 'content' 값 추출
+        content = request.POST.get('content')
 
+        post = Post.objects.create(  # 새로운 Post 
+            title = title,
+            content = content,
+        )
+        return redirect('list')   #사용자를 특정 url로 이동 (우리는 list.html로 이동시키는 거임)
+    return render(request, 'create.html')  #보여주는 거
+
+def detail(request, id):
+    post = get_object_or_404(Post, id = id) #사용자가 요청한 id를 가진 Post 데이터를 DB에서 찾음, django가 찾아야 할 데이터의 id와 사용자가 요청한 id값이 같은지 확인
+    return render(request, 'detail.html', {'post' : post})
+
+def update(request, id):
+    post = get_object_or_404(Post, id = id)
+    if request.method == "POST":
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('detail', id)
+    return render(request, 'update.html', {'post':post})
+
+def delete(request, id):
+    post = get_object_or_404(Post, id = id)
+    post.delete() 
+    return redirect('list')
